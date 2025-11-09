5ea52af28f654fe4494b0bc690bd1a8faa689ec5
akbar-farajov <akbarfarajov74@gmail.com>
2025-11-10 01:47:24 +0400

    Remove Spinner from Loading component in chat, simplifying the return to null for cleaner code.

--- src/app/(chat)/loading.tsx
+++ src/app/(chat)/loading.tsx
@@ -1,10 +1,4 @@
-import { Spinner } from "@/components/ui/spinner";
-
 const Loading = () => {
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      <Spinner className="size-10" />
-    </div>
-  );
+  return null;
 };
 export default Loading;
