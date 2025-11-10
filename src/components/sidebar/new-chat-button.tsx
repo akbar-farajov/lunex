1b9f0dee001726482f65186f14c44fbefa355bd8
akbar-farajov <akbarfarajov74@gmail.com>
2025-11-10 22:16:31 +0400

    Refactor NewChatButton to remove router dependency, simplifying the link behavior for creating a new chat.

--- src/components/sidebar/new-chat-button.tsx
+++ src/components/sidebar/new-chat-button.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { SquarePenIcon } from "lucide-react";
 import { SidebarMenuButton } from "../ui/sidebar";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
-export const NewChatButton = () => {
-  const router = useRouter();
 
+export const NewChatButton = () => {
   return (
     <SidebarMenuButton asChild>
-      <Link href="/" onClick={() => router.refresh()}>
+      <Link href="/">
         <SquarePenIcon />
         <span>New Chat</span>
       </Link>
